55f36f8e1a5b3938e6990ef4e94339db80b99794
Мария Журинова <Zhurinova_2003@mail.ru>
2022-09-14 14:37:40 +0300

    Add project files.

---
+++ lab1/lab1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+void menu(){
+	system("cls"); 
+	cout << "0.Exit" << endl;
+	cout << "1.Add pipe" << endl;
+	cout << "2.Add KC" << endl;
+	cout << "3.See all objects" << endl;
+	cout << "4.Edit pipe" << endl;
+	cout << "5.Edit KC" << endl;
+	cout << "6.Save" << endl;
+	cout << "7.Download" << endl;	
+}	
+
+int main()
+{
+}
+//	
+//int number;
+//	cin >> number;
+//	switch (number)
+//	{
+//	case 0:
+//		return 0;
+//	case 1:
+//		
+//	};
+//}
+
